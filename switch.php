26a628b6fbd0c3fd5b061ad383d54a8f9faaecfd
Jonny Dailey - work <jonny@outsidemayberry.com>
2014-10-06 22:13:05 -0400

    Added switch statement file to change the name on the results page based on the id variable from the database.

---
+++ switch.php
@@ -0,0 +1,63 @@
+<?php
+
+
+switch ($product) {
+	case 4:
+		$product = "iGrill mini";
+		break;
+	case 5:
+		$product = "Standard Meat Probe";
+		break;
+	case 6:
+		$product = "Pro Meat Probe";
+		break;
+	case 7:
+		$product = "iGrill2";
+		break;
+	case 8:
+		$product = "iGrill<sup>2</sup> Base";
+		break;
+	case 9:
+		$product = "iGrill<sup>2</sup> Battery Cover";
+		break;
+	case 10:
+		$product = "Double A Batteries";
+		break;
+	case 11:
+		$product = "iGrill mini battery cover";
+		break;
+	case 12:
+		$product = "Coin cell battery";
+		break;
+	case 13:
+		$product = "Probe Wrap";
+		break;
+	case 14:
+		$product = "iGrill mini QA";
+		break;
+	case 15:
+		$product = "iShower Door Lock";
+		break;
+	case 16:
+		$product = "Magnetic Disc";
+		break;
+	case 17:
+		$product = "Probe indicator";
+		break;
+	case 18:
+		$product = "Grate clip";
+		break;
+	case 19:
+		$product = "iGrill mini base";
+		break;
+	case 20:
+		$product = "Kitchen Thermometer Base";
+		break;
+	case 21:
+		$product = "Kitchen Thermometer Mini Base";
+		break;
+	default:
+		echo "Nothing selected";
+		break;
+}
+?>
