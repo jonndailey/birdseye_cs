bd9a5affc25f01228f4915795ec20319d2e19c0b
Jonny Dailey - work <jonny@outsidemayberry.com>
2014-10-15 01:46:44 -0400

    Added notes to the results page. If a product is checked out with a note it will show on the results page directly under the the associated record.

--- results.php
+++ results.php
@@ -1,8 +1,9 @@
 <DOCTYPE html>
+
 <title>Check out</title>
 <link rel="stylesheet" type="text/css" href="styles/glance.css">
 <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-
+<body>
 <?php
 
 $product = $_GET['id'];
@@ -30,21 +31,29 @@ echo "</div>";
 
 
 echo "<div id=\"product_results\">";
-echo "<table>" . "<th>Ticket Number</th>" . "<th>Customer Name</th>" . "<th>Date Sent</th>" . "<th>Outgoing Barcode</th>" . "<th>Incoming Barcode</th>" ;
+echo "<table>" . "<th>Ticket Number</th>" . "<th>Customer Name</th>" . "<th>Date Sent</th>" . "<th>Outgoing Barcode</th>" . "<th>Incoming Barcode</th>"  ;
 
 //Display the info grabbed from the tables displayed in HTML
 while($row = mysqli_fetch_array($result)) {
-	echo "<div id=\"glance-results\"><tr><td><a href=\"delete_entry.php?id=" . $row['id'] . "\">[X]&nbsp;</a>" . $row['ticket_number'] . "</td>";
-	echo "<td>" . $row['customer_name'] . "</td>";
-	echo "<td>" . $row['date_sent'] . "</td>";
-	echo "<td class='outgoing'> <a href='https://tools.usps.com/go/TrackConfirmAction?qtc_tLabels1=" . $row['outgoing_barcode'] . "'>" . $row['outgoing_barcode'] ." </a> </td>";
-	echo "<td class='incoming'> <a href='https://tools.usps.com/go/TrackConfirmAction?qtc_tLabels1=" . $row['incoming_barcode'] . "'>" . $row['incoming_barcode'] ." </a> </td>";
-	echo "<td>";
+echo "<div class=\"container\">";
+echo "<div id=\"glance-results\"><tr><td>" . $row['ticket_number'] . "</td>";
+echo "<td>" . $row['customer_name'] . "</td>";
+echo "<td>" . $row['date_sent'] . "</td>";
+echo "<td> <a href='https://tools.usps.com/go/TrackConfirmAction?qtc_tLabels1=" . $row['outgoing_barcode'] . "'>" . $row['outgoing_barcode'] ." </a> </td>";
+echo "<td> <a href='https://tools.usps.com/go/TrackConfirmAction?qtc_tLabels1=" . $row['incoming_barcode'] . "'>" . $row['incoming_barcode'] ." </a> </td> ";
 
+
+if ($row["note"]){
+	echo "<tr><td>";
+	echo "<div class=\"thenotes\">";
+	echo "<strong>Note:&nbsp;</strong>" . $row['note'];
+	echo "</td></tr>";
+	echo "</div>";
+	}
 }
-echo "</td>";
+echo "</table>";
+
 echo "</div>";
-
-
-
 ?>
+</body>
+</html>
